d2e779a40a7148c767cff19b7c10aa17db4c5937
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2024-11-18 10:07:51 -0300

    melhorar exibição single

--- simulacao_single.py
+++ simulacao_single.py
@@ -2,17 +2,17 @@ from simulacao import simular
 from utils import media, desvio_padrao, time_format
 
 
-clientes, (_, _, _, tof) = simular(seed=42)
+clientes, (ts_med, tf_med, tsis_med, tof) = simular(seed=42)
 
 
-header = "{:<4} | {} | {:<8} | {:<20} | {:<8} | {:<8} | {:<8} | {:<8} | {:<18} | {:<8}".format(
+header = "{:<4} | {} | {:<4} | {:<20} | {:<4} | {:<6} | {:<6} | {:<5} | {:<15} | {:<8}".format(
     "#", "T", "TEC", "TCR", "TS", "TIS", "TFS", "TF", "TSis", "TOF"
 )
 print(header)
 print("-" * len(header))
 for i, c in enumerate(clientes):
     print(
-        "{:>4} | {} | {:>8} | {:>8} {:<11} | {:>8} | {:>8} | {:>8} | {:>8} | {:>8} {:<9}".format(
+        "{:>4} | {} | {:>4} | {:>8} {:<11} | {:>4} | {:>6} | {:>6} | {:>5} | {:>5} {:<9}".format(
             i + 1,
             c.tipo,
             c.tec,
@@ -30,13 +30,9 @@ for i, c in enumerate(clientes):
 print("-" * len(header))
 print(header)
 
-ts_med = media([c.ts for c in clientes])
-tf_med = media([c.tf for c in clientes])
-tsis_med = media([c.tsis for c in clientes])
-
 print("-" * len(header))
 print(
-    "médias: {:<36} {:>8.2f} {:<23} {:>8.2f}   {:>8.2f} {:<9}   {:>8.2f}".format(
+    "médias: {:<29} {:>8.2f} {:<16} {:>8.2f} {:>8.2f} {:<9} {:>8.2f}".format(
         "", ts_med, "", tf_med, tsis_med, time_format(tsis_med), tof
     )
 )
@@ -46,7 +42,7 @@ tf_dp = desvio_padrao([c.tf for c in clientes])
 tsis_dp = desvio_padrao([c.tsis for c in clientes])
 
 print(
-    "desvios: {:<35} {:>8.2f} {:<23} {:>8.2f}   {:>8.2f} {:<9}".format(
+    "desvios: {:<28} {:>8.2f} {:<16} {:>8.2f} {:>8.2f} {:<9}".format(
         "", ts_dp, "", tf_dp, tsis_dp, time_format(tsis_dp)
     )
 )
